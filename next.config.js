e048d9e0fdce5ceb19fc4acf15b2e72884b1cc58
alexstouffer <bizkite@hotmail.com>
2022-06-10 00:29:23 -0700

    update style

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      // process.env.WORDPRESS_API_URL.match(/(http(?:s)?:\/\/)(.*)/)[2], // Valid WP Image domain.
+      'picks.stoubord.com'
+    ],
+  },
+
 }
 
 module.exports = nextConfig
